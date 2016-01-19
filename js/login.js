973662ec7ad6d27cb051449455fde32a995caf40
geek <1484288448@qq.com>
2016-01-19 12:42:07 +0800

    login

--- js/login.js
+++ js/login.js
@@ -5,11 +5,14 @@ function login(){
     for (var i = 0; i < data.length; i++) {
       if(data[i].enterCode==$("#enterCode").val()){
         loginOk=true;
+        $.cookie("loginOk",true);
         location.href="index.html";
         return;
       }
     }
-    alert("Login Error...");
+
+
+    alert("Enter Error...");
 
   });
 }
